7373f95a4fe615089c65e5dce31ffc25a7ec70fb
darshinipri <darshinipd07@gmail.com>
2020-06-22 22:11:34 +0530

    javaScript Assignment

---
+++ jsassignment/func.js
@@ -0,0 +1,27 @@
+function myFunction(a, b) {
+    return a * b;
+  }
+console.log(myFunction(2,4));
+
+
+
+var x = function (a, b) {return a / b};
+var z = x(4, 3);
+console.log(z)
+
+var userName = "Bill";
+
+function display(name)
+{
+    console.log("EDISON: " + name);
+}
+
+display(userName);
+
+
+let add = (x,y) => x + y;
+console.log(add(10,20)); 
+
+hello = (val) => "Hello " + val;
+console.log(hello("priya"))
+
